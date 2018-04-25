aef1f17224d2bc96a8d01414846ea73a204e4479
aQua <aQua@aQuaYi.com>
2018-04-25 17:04:50 +0800

    lab2 开始重构程序

--- src/raft/raft_main.go
+++ src/raft/raft_main.go
@@ -20,65 +20,8 @@ package raft
 import (
 	"labrpc"
 	"math/rand"
-	"sync"
 	"time"
 )
-
-type state int
-
-// 规定了 server 所需的 3 种状态
-const (
-	LEADER state = iota
-	CANDIDATE
-	FOLLOWER
-)
-const (
-	// HBINTERVAL is haertbeat interval
-	HBINTERVAL = 50 * time.Millisecond // 50ms
-
-	// NULL 表示没有投票给任何人
-	NULL = -1
-)
-
-// Raft implements a single Raft peer.
-type Raft struct {
-	mu        sync.Mutex          // Lock to protect shared access to this peer's state
-	peers     []*labrpc.ClientEnd // RPC end points of all peers
-	persister *Persister          // Object to hold this peer's persisted state
-	me        int                 // this peer's index into peers[]
-
-	// NOTICE: Your data here (2A, 2B, 2C).
-	// Look at the paper's Figure 2 for a description of what
-	// state a Raft server must maintain.
-
-	// Persistent state on call servers
-	currentTerm int        // 此 server 当前所处的 term 编号
-	votedFor    int        // 此 server 在此 term 中投票给了谁，是 peers 中的索引号
-	logs        []LogEntry // 此 server 中保存的 logs
-
-	// Volatile state on all servers:
-	commitIndex int // logs 中已经 commited 的 log 的最大索引号
-	lastApplied int // logs 中最新元素的索引号
-
-	// Volatile state on leaders:
-	nextIndex  []int // 下一个要发送给 follower 的 log 的索引号
-	matchIndex []int // leader 与 follower 共有的 log 的最大的索引号
-
-	state    state
-	t        *time.Timer
-	cond     *sync.Cond
-	shutdown chan struct{}
-
-	// state     state
-	// voteCount int
-
-	// 	chanCommit    chan struct{}
-	// 	chanHeartbeat chan struct{}
-	// 	chanGrantVote chan struct{}
-	// 	chanLeader    chan struct{}
-	// 	chanApply     chan ApplyMsg
-
-}
 
 // GetState 可以获取 raft 对象的状态
 // return currentTerm and whether this server
@@ -221,26 +164,7 @@ func (rf *Raft) canvass() {
 //
 func Make(peers []*labrpc.ClientEnd, me int,
 	persister *Persister, applyCh chan ApplyMsg) *Raft {
-	rf := &Raft{}
-	rf.peers = peers
-	rf.persister = persister
-	rf.me = me
-
-	// Your initialization code here (2A, 2B, 2C).
-
-	// currentTerm
-	rf.currentTerm = 0
-	// votedFor
-	rf.votedFor = NULL
-	//log
-	rf.logs = make([]LogEntry, 1)
-
-	rf.commitIndex = 0
-	rf.lastApplied = 0
-
-	rf.state = FOLLOWER
-	rf.cond = sync.NewCond(&rf.mu)
-	rf.shutdown = make(chan struct{})
+	rf := newRaft(peers, me, persister, applyCh)
 
 	timeout := time.Duration(300 + rand.Int31n(400))
 	rf.t = time.NewTimer(timeout * time.Millisecond)
---
+++ src/raft/raft_new_test.go
@@ -0,0 +1,39 @@
+package raft
+
+import "testing"
+
+func Test_state_String(t *testing.T) {
+	tests := []struct {
+		name string
+		s    state
+		want string
+	}{
+
+		{
+			"print leader",
+			LEADER,
+			"Leader",
+		},
+
+		{
+			"print candidate",
+			CANDIDATE,
+			"Candidate",
+		},
+
+		{
+			"print follower",
+			FOLLOWER,
+			"Follower",
+		},
+
+		//
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.s.String(); got != tt.want {
+				t.Errorf("state.String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
