1a7b5ef501cf072b8bf349d198bb6d12e2f79c0d
aQua <aQua@aQuaYi.com>
2018-04-25 17:09:31 +0800

    lab2 继续重构

--- src/raft/raft_candidate.go
+++ src/raft/raft_candidate.go
@@ -1,1 +1,27 @@
 package raft
+
+// rf 为自己拉票，以便赢得选举
+func (rf *Raft) canvass() {
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+
+	args := RequestVoteArgs{
+		Term:         rf.currentTerm,
+		CandidateID:  rf.me,
+		LastLogTerm:  rf.getLastTerm(),
+		LastLogIndex: rf.getLastIndex(),
+	}
+
+	for i := range rf.peers {
+		if i != rf.me && rf.state == CANDIDATE {
+			go func(i int) {
+				var reply RequestVoteReply
+				rf.sendRequestVote(i, &args, &reply)
+
+				// NOTICE: 后续如何处理
+			}(i)
+		}
+	}
+
+	return
+}
---
+++ src/raft/raft_method.go
@@ -0,0 +1,103 @@
+package raft
+
+// GetState 可以获取 raft 对象的状态
+// return currentTerm and whether this server
+// believes it is the leader.
+func (rf *Raft) GetState() (int, bool) {
+	var term int
+	var isleader bool
+	// NOTICE: Your code here (2A).
+
+	// TODO: 这里为什么要上锁呢
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+
+	term = rf.currentTerm
+	isleader = rf.state == LEADER
+
+	return term, isleader
+}
+
+func (rf *Raft) getLastIndex() int {
+	return rf.logs[len(rf.logs)-1].LogIndex
+}
+
+func (rf *Raft) getLastTerm() int {
+	return rf.logs[len(rf.logs)-1].LogTerm
+}
+
+//
+// save Raft's persistent state to stable storage,
+// where it can later be retrieved after a crash and restart.
+// see paper's Figure 2 for a description of what should be persistent.
+//
+func (rf *Raft) persist() {
+	// NOTICE: Your code here (2C).
+	// Example:
+	// w := new(bytes.Buffer)
+	// e := labgob.NewEncoder(w)
+	// e.Encode(rf.xxx)
+	// e.Encode(rf.yyy)
+	// data := w.Bytes()
+	// rf.persister.SaveRaftState(data)
+}
+
+//
+// restore previously persisted state.
+//
+func (rf *Raft) readPersist(data []byte) {
+	if data == nil || len(data) < 1 { // bootstrap without any state?
+		return
+	}
+	// NOTICE: Your code here (2C).
+	// Example:
+	// r := bytes.NewBuffer(data)
+	// d := labgob.NewDecoder(r)
+	// var xxx
+	// var yyy
+	// if d.Decode(&xxx) != nil ||
+	//    d.Decode(&yyy) != nil {
+	//   error...
+	// } else {
+	//   rf.xxx = xxx
+	//   rf.yyy = yyy
+	// }
+}
+
+// Start 启动
+// the service using Raft (e.g. a k/v server) wants to start
+// agreement on the next command to be appended to Raft's log. if this
+// server isn't the leader, returns false. otherwise start the
+// agreement and return immediately. there is no guarantee that this
+// command will ever be committed to the Raft log, since the leader
+// may fail or lose an election.
+//
+// the first return value is the index that the command will appear at
+// if it's ever committed. the second return value is the current
+// term. the third return value is true if this server believes it is
+// the leader.
+//
+func (rf *Raft) Start(command interface{}) (int, int, bool) {
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+	index := -1
+	term := rf.currentTerm
+	isLeader := rf.state == LEADER
+	if isLeader {
+		index = rf.getLastIndex() + 1
+		//fmt.Printf("raft:%d start\n",rf.me)
+		rf.logs = append(rf.logs, LogEntry{LogTerm: term, LogCmd: command, LogIndex: index}) // append new entry from client
+		rf.persist()
+	}
+	return index, term, isLeader
+}
+
+// Kill is
+// the tester calls Kill() when a Raft instance won't
+// be needed again. you are not required to do anything
+// in Kill(), but it might be convenient to (for example)
+// turn off debug output from this instance.
+//
+func (rf *Raft) Kill() {
+	// NOTICE: Your code here, if desired.
+}
