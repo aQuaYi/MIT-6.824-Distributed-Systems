4f610094da485aa8a59233eb1a9d4f4776b615e9
aQua <aQua@aQuaYi.com>
2018-04-26 10:04:16 +0800

    lab2 开始重构

--- src/raft/util.go
+++ src/raft/util.go
@@ -3,7 +3,7 @@ package raft
 import "log"
 
 // needDebug for Debugging
-const needDebug = true
+const needDebug = false
 
 // DPrintf 根据设置打印输出
 func DPrintf(format string, a ...interface{}) (n int, err error) {
