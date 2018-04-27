0f29d015aea93af4d89247932207d961f810dc13
aQua <aQua@aQuaYi.com>
2018-04-27 10:37:44 +0800

    lab2 开始输出 debug 信息

--- src/raft/util.go
+++ src/raft/util.go
@@ -3,7 +3,7 @@ package raft
 import "log"
 
 // needDebug for Debugging
-const needDebug = false
+const needDebug = true
 
 // debugPrintf 根据设置打印输出
 func debugPrintf(format string, a ...interface{}) {
