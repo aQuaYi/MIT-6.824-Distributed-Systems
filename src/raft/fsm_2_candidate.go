f2ccfd23ef15fdac125c86ba1679ab3b53cc777a
aQua <aQua@aQuaYi.com>
2018-05-03 16:07:42 +0800

    lab2a ok lab2b fail

--- src/raft/fsm_2_candidate.go
+++ src/raft/fsm_2_candidate.go
@@ -16,11 +16,10 @@ func comeToPower(rf *Raft, args interface{}) fsmState {
 
 	// 新当选的 Leader 需要重置以下两个属性
 	rf.nextIndex = make([]int, len(rf.peers))
-	rf.matchIndex = make([]int, len(rf.peers))
-	for i := 0; i < len(rf.peers); i++ {
+	for i := range rf.nextIndex {
 		rf.nextIndex[i] = len(rf.logs)
-		rf.matchIndex[i] = 0
 	}
+	rf.matchIndex = make([]int, len(rf.peers))
 
 	go sendHeartbeat(rf)
 
@@ -66,6 +65,26 @@ func makeHeartbeat(rf *Raft) {
 				return
 			}
 
+			if reply.Term > rf.currentTerm {
+				go rf.call(discoverNewTermEvent, toFollowerArgs{
+					term:     reply.Term,
+					votedFor: NULL,
+				})
+				return
+			}
+
+			// if get an old RPC reply
+			// TODO: 为什么接收到一个 old rpc reply
+			if args.Term != rf.currentTerm {
+				// rf.rwmu.Unlock()
+				return
+			}
+
+			if rf.state != LEADER {
+				// rf.rwmu.Unlock()
+				return
+			}
+
 			rf.rwmu.Lock()
 			defer rf.rwmu.Unlock()
 
@@ -75,109 +94,97 @@ func makeHeartbeat(rf *Raft) {
 			// 2) if AppendEntries fails because of log inconsistency:
 			//    decrement nextIndex and retry
 
-			// if get an old RPC reply
-			// TODO: 为什么接收到一个 old rpc reply
+			// // check matchIndex in case RPC is lost during reply of log recovery
+			// // log is attached to follower but leader not receive success reply
+			// if reply.Success {
+			// 	if rf.matchIndex[server] < args.PrevLogIndex {
+			// 		rf.matchIndex[server] = args.PrevLogIndex
+			// 		rf.appendedNewEntriesChan <- struct{}{}
+			// 	}
+			// 	return
+			// }
 
-			if args.Term != rf.currentTerm {
-				// rf.rwmu.Unlock()
-				return
-			}
-
-			// check matchIndex in case RPC is lost during reply of log recovery
-			// log is attached to follower but leader not receive success reply
 			if reply.Success {
-				if rf.matchIndex[server] < args.PrevLogIndex {
-					rf.matchIndex[server] = args.PrevLogIndex
-					rf.appendedNewEntriesChan <- struct{}{}
-				}
-				return
+				rf.nextIndex[server] = max(rf.nextIndex[server], reply.NextIndex)
+				rf.matchIndex[server] = rf.nextIndex[server] - 1
+				rf.appendedNewEntriesChan <- struct{}{}
+			} else {
+				rf.nextIndex[server] = min(rf.nextIndex[server], reply.NextIndex)
 			}
 
-			if rf.state != LEADER {
-				// rf.rwmu.Unlock()
-				return
-			}
+			// var firstTermIndex int
+			// rf.nextIndex[server] = reply.NextIndex
+			// for {
+			// 	debugPrintf("abc:%v, server: %v reply: %v\n", rf, server, reply)
+			// 	detectAppendEntriesArgs := newAppendEntriesArgs(rf, server)
+			// 	detectReply := new(AppendEntriesReply)
 
-			if reply.Term > rf.currentTerm {
-				go rf.call(discoverNewTermEvent, toFollowerArgs{
-					term:     reply.Term,
-					votedFor: NULL,
-				})
-				return
-			}
+			// 				rf.rwmu.Unlock()
+			// 				ok := rf.sendAppendEntries(server, detectAppendEntriesArgs, detectReply)
+			// 				rf.rwmu.Lock()
+			// //
+			// 				if !ok {
+			// 					debugPrintf("[server: %v]not receive from %v\n", rf.me, server)
+			// 					rf.nextIndex[server] = len(rf.logs)
+			// 					return
+			// 				}
+			// //
+			// 				if args.Term != rf.currentTerm {
+			// 					return
+			// 				}
+			// //
+			// 				if detectReply.Term > rf.currentTerm {
+			// 					go rf.call(discoverNewTermEvent, toFollowerArgs{
+			// 						term:     reply.Term,
+			// 						votedFor: NULL,
+			// 					})
+			// 					return
+			// 				}
+			// //
+			// 				if detectReply.Success {
+			// 					firstTermIndex = detectAppendEntriesArgs.PrevLogIndex + 1
+			// 					break
+			// 				}
+			// //
+			// 				rf.nextIndex[server] = detectReply.NextIndex
+			// 			}
+			// //
+			// 			debugPrintf("[server: %v]Consistency check: server: %v, firstTermIndex: %v", rf.me, server, firstTermIndex)
+			// 			forceAppendEntriesArgs := newAppendEntriesArgs(rf, server)
+			// 			forceReply := new(AppendEntriesReply)
+			// //
+			// 			rf.rwmu.Unlock()
+			// 			ok = rf.sendAppendEntries(server, forceAppendEntriesArgs, forceReply)
+			// 			rf.rwmu.Lock()
+			// //
+			// 			if !ok {
+			// 				debugPrintf("[server: %v]no reponse from %v\n", rf.me, server)
+			// 				rf.nextIndex[server] = len(rf.logs)
+			// 				return
+			// 			}
+			// //
+			// 			if args.Term != rf.currentTerm {
+			// 				// rf.rwmu.Unlock()
+			// 				return
+			// 			}
+			// //
+			// 			if forceReply.Term > rf.currentTerm {
+			// 				go rf.call(discoverNewTermEvent, toFollowerArgs{
+			// 					term:     reply.Term,
+			// 					votedFor: NULL,
+			// 				})
+			// 				return
+			// 			}
+			// //
+			// 			debugPrintf("[server: %v]successfully append entries: %v\n", rf.me, forceReply)
+			// 			rf.nextIndex[server] = len(rf.logs)
+			// 			rf.matchIndex[server] = forceAppendEntriesArgs.PrevLogIndex + len(forceAppendEntriesArgs.Entries)
+			// 			// rf.rwmu.Unlock()
+			// 			rf.appendedNewEntriesChan <- struct{}{}
+			// 			return
+			// //
+			// 			// rf.rwmu.Unlock()
 
-			var firstTermIndex int
-			rf.nextIndex[server] = reply.NextIndex
-			for {
-				debugPrintf("abc:%v, server: %v reply: %v\n", rf, server, reply)
-				detectAppendEntriesArgs := newAppendEntriesArgs(rf, server)
-				detectReply := new(AppendEntriesReply)
-
-				rf.rwmu.Unlock()
-				ok := rf.sendAppendEntries(server, detectAppendEntriesArgs, detectReply)
-				rf.rwmu.Lock()
-
-				if !ok {
-					debugPrintf("[server: %v]not receive from %v\n", rf.me, server)
-					rf.nextIndex[server] = len(rf.logs)
-					return
-				}
-
-				if args.Term != rf.currentTerm {
-					return
-				}
-
-				if detectReply.Term > rf.currentTerm {
-					go rf.call(discoverNewTermEvent, toFollowerArgs{
-						term:     reply.Term,
-						votedFor: NULL,
-					})
-					return
-				}
-
-				if detectReply.Success {
-					firstTermIndex = detectAppendEntriesArgs.PrevLogIndex + 1
-					break
-				}
-
-				rf.nextIndex[server] = detectReply.NextIndex
-			}
-
-			debugPrintf("[server: %v]Consistency check: server: %v, firstTermIndex: %v", rf.me, server, firstTermIndex)
-			forceAppendEntriesArgs := newAppendEntriesArgs(rf, server)
-			forceReply := new(AppendEntriesReply)
-
-			rf.rwmu.Unlock()
-			ok = rf.sendAppendEntries(server, forceAppendEntriesArgs, forceReply)
-			rf.rwmu.Lock()
-
-			if !ok {
-				debugPrintf("[server: %v]no reponse from %v\n", rf.me, server)
-				rf.nextIndex[server] = len(rf.logs)
-				return
-			}
-
-			if args.Term != rf.currentTerm {
-				// rf.rwmu.Unlock()
-				return
-			}
-
-			if forceReply.Term > rf.currentTerm {
-				go rf.call(discoverNewTermEvent, toFollowerArgs{
-					term:     reply.Term,
-					votedFor: NULL,
-				})
-				return
-			}
-
-			debugPrintf("[server: %v]successfully append entries: %v\n", rf.me, forceReply)
-			rf.nextIndex[server] = len(rf.logs)
-			rf.matchIndex[server] = forceAppendEntriesArgs.PrevLogIndex + len(forceAppendEntriesArgs.Entries)
-			// rf.rwmu.Unlock()
-			rf.appendedNewEntriesChan <- struct{}{}
-			return
-
-			// rf.rwmu.Unlock()
 		}(server)
 	}
 
